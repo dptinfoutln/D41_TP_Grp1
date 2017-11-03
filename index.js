9bb4ef1cd7b7bc9abe1af45f7482b37fb61d6a97
Damien L <damien-lemenager@etud.univ-tln.fr>
2017-11-03 14:35:30 +0100

    add skeleton

---
+++ index.js
@@ -0,0 +1,38 @@
+// ----------------------- CAMERA -----------------------
+
+// A COMPLETER 
+
+// ----------------------- GEOLOCALISATION -----------------------
+
+// API KEYS AND URLs
+// var OpenWeatherAppKey = "6dba452732858e697a2794301a080e0e";
+// var queryString =
+//     'http://api.openweathermap.org/data/2.5/weather?lat='
+//     + latitude + '&lon=' + longitude + '&appid=' + OpenWeatherAppKey + '&units=imperial';
+
+// var FlickrAPIkey = "2c967da11ad4ab0974ee5461dfd3935c"
+// var queryString =
+//     "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FlickrAPIkey + "&lat="
+//     + latitude + "&lon=" + longitude + "&format=json&jsoncallback=?";
+
+// A COMPLETER
+
+// ----------------------- AUDIO -----------------------
+
+// A COMPLETER
+
+// ----------------------- ACCESS BDD -----------------------
+
+// SQL QUERIES
+// var sqlDropTable = 'DROP TABLE IF EXISTS pictures'
+// var sqlCreateTable = 'CREATE TABLE IF NOT EXISTS pictures ( id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT ) ';
+
+// A COMPLETER
+
+// ----------------------- CONTROLER -----------------------
+
+$(document).ready(function() {
+
+  // A COMPLETER
+
+})
